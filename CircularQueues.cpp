4cae975681b9e29100893e35f7b5bd2ec472579a
Daffansyah276 <daffansyaharya@gmail.com>
2025-06-12 10:46:43 +0700

    membuat fungsi kembali ke awal

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -35,4 +35,14 @@ public:
             FRONT = 0; // 2.a
             REAR = 0;  // 2.b
         }
-        
+        else
+        {
+            // jika REAR berada di posisi terakhir Array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+   
+    
