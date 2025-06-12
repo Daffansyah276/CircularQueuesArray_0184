489cbed0d4c9e2308bec48bf9f1684c4d017dcd8
Daffansyah276 <daffansyaharya@gmail.com>
2025-06-12 11:09:59 +0700

    membuat switch default

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -139,4 +139,10 @@ int main() {
             case '4': {
                 return 0;
             }
-         
+            default: {
+                cout << "Invalid Option!!" << endl;
+                break;
+            }
+            }
+        }
+        
