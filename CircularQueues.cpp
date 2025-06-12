42dc275fa0e4ca2ad11feb100620392162226ca0
Daffansyah276 <daffansyaharya@gmail.com>
2025-06-12 10:27:54 +0700

    membuat class public

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -6,4 +6,15 @@ class Queues
 private:
     static const int max = 5;
     int FRONT, REAR;
-    int queue_array[5]
+    int queue_array[5];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+
+
+};
