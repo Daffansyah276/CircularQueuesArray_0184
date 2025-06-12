385684e852b2c9c990aedb2576be2e149a24fa3e
Daffansyah276 <daffansyaharya@gmail.com>
2025-06-12 11:09:17 +0700

    membuat switch case 3

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -132,4 +132,8 @@ int main() {
                 q.remove();
                 break;
             }
-  
+            case '3': {
+                q.display();
+                break;
+            }
+         
